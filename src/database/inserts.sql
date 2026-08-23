0f43059d1a34947495abc0a5b326471effe3f930
Santatriniaina Rahantanirina <hei.santatriniaina.2@gmail.com>
2026-08-23 12:03:11 +0300

    feat : creation and insertion of  tables users, courses, exam, questions, choices, attempts and aswers

---
+++ src/database/inserts.sql
@@ -0,0 +1,24 @@
+
+
+INSERT INTO users(name, email, password, role,is_active)
+            VALUES('Randria', 'Hery', 'hery@gmail.com', 'secure_pass', 'admin', TRUE);
+
+INSERT INTO users(name, email, password, role,is_active)
+            VALUES('Rakoto', 'Mamy', 'mamy@gmail.com', 'secure_pass', 'student', TRUE);
+
+            
+INSERT INTO users(name, email, password, role,is_active)
+            VALUES('Fitia', 'Anja', 'anja@gmail.com', 'secure_pass', 'student', TRUE);
+            
+
+            
+
+INSERT INTO courses(name, description)
+            VAUES('PROG_2', 'Encapsulation');
+        
+INSERT INTO courses(name, description)
+            VAUES('SYS_2', 'Apprendre le subnetting');
+
+            
+INSERT INTO courses(name, description)
+            VAUES('WEB_2', 'React');
