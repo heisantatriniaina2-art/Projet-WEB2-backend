04976d0451886c078e85e16f2f4bd91533aed34c
Santatriniaina Rahantanirina <hei.santatriniaina.2@gmail.com>
2026-08-24 19:05:46 +0300

    fix(backend): install cors for frontend connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { authController } from './controllers/authController.js';
+
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.post('/api/auth/login', authController.login);
 
