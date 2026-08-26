1563e03e5bd9a01d858afc3ac338ebc3442bedc8
Santatriniaina Rahantanirina <hei.santatriniaina.2@gmail.com>
2026-08-26 17:39:56 +0300

    refactor: rename database folder, update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@ import express from 'express';
 import cors from 'cors';
 import { authController } from './controllers/authController.js';
 
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Exam Hub API fonctionne !' });
+});
 
 app.post('/api/auth/login', authController.login);
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Exam Hub API running on http://localhost:${PORT}`);
 });
